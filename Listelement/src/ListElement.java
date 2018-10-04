ec664d28fdf6e19c3e9454ed63cab33f4c9acb01
Mohamed Aly <mma012@uregina.ca>
2018-10-04 00:00:31 -0600

    Creating the ListElement source file

---
+++ Listelement/src/ListElement.java
@@ -0,0 +1,16 @@
+
+public class ListElement {
+
+	
+	private ListElement next ;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement ()
+	
+	{
+	 this.data = 0; 
+	 this.next = null;
+	 this.previous = null;	 
+	}
+}
